3747d825a123965334e9e4cd3cabfa591c1fcd3e
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-20 01:49:45 -0400

    Mikey was here

--- app/src/main/java/xyz/michaelzhao/postup/MainActivity.java
+++ app/src/main/java/xyz/michaelzhao/postup/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Mikey was here
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
